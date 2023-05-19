4c454433fd46fa73ca58d1a787e42147d81078eb
barry <barry.klein@student.moringaschool.com>
2023-05-19 12:08:44 +0300

    temperature  code and it works

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -10,22 +10,31 @@ def admin_login(username, password): ###def is a keyword (kuonyesha start of a f
         return "Access granted"
     else:
         return "Access denied" ##>>>> this returns is used to  specify the value that the function  should give 
-        ##  when the function is called
+        ##  when the function is called  the given out put is given and the input 
  
 print(admin_login("admin", "12345"))   
 print(admin_login("ADMIN", "12345"))    
 
 
 
+def hows_the_weather(temperature): #### argument, a temperature
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature >= 40 and temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
 
-def hows_the_weather(temperature):
-    # your code here
-    pass
+print(hows_the_weather(33))    
+print(hows_the_weather(99))    
+print(hows_the_weather(75))    
 
-def fizzbuzz(num):
-    # your code here
-    pass
+# def fizzbuzz(num):
+#     # your code here
+#     pass
 
-def calculator(operation, num1, num2):
-    # your code here
-    pass
+# def calculator(operation, num1, num2):
+#     # your code here
+#     pass
